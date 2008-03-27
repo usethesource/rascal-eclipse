2ce6dc12995b4910d925f79a078d60a1b3c8c532
Robert M. Fuhrer <rmfuhrer@optonline.net>
2008-03-27 20:52:17 +0000

    Initial import of Program Fact Database browser.

---
+++ src/org/eclipse/imp/pdb/ui/PDBUIPlugin.java
@@ -0,0 +1,50 @@
+package org.eclipse.imp.pdb.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class PDBUIPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.imp.pdb.ui";
+
+	// The shared instance
+	private static PDBUIPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public PDBUIPlugin() {
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static PDBUIPlugin getDefault() {
+		return plugin;
+	}
+
+}
