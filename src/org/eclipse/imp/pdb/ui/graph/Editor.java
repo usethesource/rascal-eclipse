32ebae4b3cf4b5068357402afb8fbfc65a7301f5
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2009-02-02 14:21:20 +0000

    added method for conveniently popping up a visualization

--- src/org/eclipse/imp/pdb/ui/graph/Editor.java
+++ src/org/eclipse/imp/pdb/ui/graph/Editor.java
@@ -1,13 +1,20 @@
 package org.eclipse.imp.pdb.ui.graph;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.ui.PDBUIPlugin;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.EditorPart;
 import org.eclipse.zest.core.widgets.Graph;
 import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
@@ -20,6 +27,30 @@ public class Editor extends EditorPart {
 	public Editor() {
 	}
 
+	public static void open(final IValue value) {
+		if (value == null) {
+			return;
+		}
+	 	IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+
+		if (win != null) {
+			final IWorkbenchPage page = win.getActivePage();
+
+			if (page != null) {
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						try {
+							page.openEditor(new ValueEditorInput(value), Editor.EditorId);
+						} catch (PartInitException e) {
+							// TODO Auto-generated catch block
+						}
+					}
+				});
+			}
+		}
+	}
+	
 	@Override
 	public void doSave(IProgressMonitor monitor) {
 	}
