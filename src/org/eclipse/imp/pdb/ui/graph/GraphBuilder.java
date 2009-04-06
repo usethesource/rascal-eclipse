083da1e50c951424b400092792624cc827235ee1
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2009-04-06 14:42:08 +0000

    graph can now visualize any data

--- src/org/eclipse/imp/pdb/ui/graph/GraphBuilder.java
+++ src/org/eclipse/imp/pdb/ui/graph/GraphBuilder.java
@@ -1,8 +1,13 @@
 package org.eclipse.imp.pdb.ui.graph;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.IMap;
+import org.eclipse.imp.pdb.facts.INode;
 import org.eclipse.imp.pdb.facts.IRelation;
+import org.eclipse.imp.pdb.facts.ISet;
 import org.eclipse.imp.pdb.facts.ITuple;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.type.Type;
@@ -12,7 +17,7 @@ import org.eclipse.zest.core.widgets.GraphConnection;
 import org.eclipse.zest.core.widgets.GraphNode;
 
 public class GraphBuilder {
-	private HashMap<String, GraphNode> fNodeCache = new HashMap<String, GraphNode>();
+	private HashMap<IValue, GraphNode> fNodeCache = new HashMap<IValue, GraphNode>();
 	private final Graph graph;
 	
 	public GraphBuilder(Graph graph) {
@@ -20,48 +25,136 @@ public class GraphBuilder {
 	}
 
 	public void computeGraph(IValue fact) {
-    	if (canShow(fact.getType())) {
-    		convertBinaryRelToGraph((IRelation) fact);
-        }
+		fNodeCache.clear();
+    	convert(fact);
     }
 	
-	public static boolean canShow(Type type) {
-		return type.isRelationType() && type.getArity() == 2;
+	private GraphNode convert(IValue fact) {
+		Type type = fact.getType();
+		
+		if (type.isRelationType()) {
+			return convertRelation(fact);
+		}
+		else if (type.isSetType()) {
+			return convertIterator(((ISet) fact).iterator(), fact, "{...}");
+		}
+		else if (type.isListType()) {
+			return convertIterator(((IList) fact).iterator(), fact, "[...]");
+		}
+		else if (type.isNodeType() || type.isAbstractDataType() || type.isConstructorType()) {
+			return convertTree((INode) fact);
+		}
+		else if (type.isTupleType()) {
+			return convertTuple((ITuple) fact);
+		}
+		else if (type.isMapType()) {
+			return convertMap((IMap) fact);
+		}
+		else {
+			return getOrCreateNode(fact, fact.toString());
+		}
 	}
 
-    private void convertBinaryRelToGraph(IRelation rel) {
-        fNodeCache.clear();
+	private GraphNode convertMap(IMap fact) {
+		GraphNode root = getOrCreateNode(fact, "(...)");
+		
+		for (IValue key : fact) {
+	        GraphNode from = convert(key);
+	        GraphNode to = convert(fact.get(key));
 
-        for (IValue value : rel) {
-        	ITuple tuple = (ITuple) value;
-            GraphNode from = getOrCreateNode(tuple.get(0));
-            GraphNode to = getOrCreateNode(tuple.get(1));
+	        if (from != null && to != null) {
+	        	new GraphConnection(graph, SWT.NONE, root, from);
+	            new GraphConnection(graph, SWT.NONE, from, to);
+	        }
+	    }
+		
+		return root;
+	}
 
-            if (from != null && to != null) {
-                new GraphConnection(graph, SWT.NONE, from, to);
-            }
-        }
-    }
+	private GraphNode convertTuple(ITuple fact) {
+		GraphNode node = getOrCreateNode(fact, "<...>");
+		
+		for (int i = 0; i < fact.arity(); i++) {
+			new GraphConnection(graph, SWT.NONE, node, convert(fact.get(i)));
+		}
+		
+		return node;
+	}
 
-    private GraphNode getOrCreateNode(IValue value) {
-    	String nodeName = getNodeName(value);
-        GraphNode node;
+	private GraphNode convertTree(INode fact) {
+		GraphNode node = getOrCreateNode(fact, fact.getName());
+		
+		for (IValue child : fact) {
+			new GraphConnection(graph, SWT.NONE, node, convert(child));
+		}
+		
+		return node;
+	}
 
-        if (nodeName.length() == 0) {
-            return null;
-        }
+	private GraphNode convertRelation(IValue fact) {
+		Type type = fact.getType();
+		IRelation rel = (IRelation) fact;
+		
+		if (type.getArity() == 2) {
+			for (IValue value : rel) {
+		    	ITuple tuple = (ITuple) value;
+		        GraphNode from = convert(tuple.get(0));
+		        GraphNode to = convert(tuple.get(1));
 
-        if (fNodeCache.containsKey(nodeName)) {
-            node = fNodeCache.get(nodeName);
-        } else {
-            node = new GraphNode(graph, SWT.NONE, nodeName);
-            fNodeCache.put(nodeName, node);
+		        if (from != null && to != null) {
+		            new GraphConnection(graph, SWT.NONE, from, to);
+		        }
+		    }
+			
+			GraphNode root = getOrCreateNode(fact, "{...}");
+			ISet top = rel.domain().subtract(rel.range());
+			
+			for (IValue elem : top) {
+				GraphNode to = convert(elem);
+				new GraphConnection(graph, SWT.NONE, root, to);
+			}
+			
+			return root;
+		}
+		else if (rel.arity() == 3) { // assume labeled graph
+			return convertRelation(rel.select(0,2));
+		}
+		else {
+			return convertSet(fact);
+		}
+			
+	}
+
+	private GraphNode convertSet(IValue fact) {
+		return convertIterator(((ISet) fact).iterator(), fact, "{...}");
+	}
+
+	private GraphNode convertIterator(Iterator<IValue> iterator, IValue value, String label) {
+		GraphNode root = getOrCreateNode(value, label);
+		
+		while (iterator.hasNext()) {
+			IValue e = iterator.next();
+			new GraphConnection(graph, SWT.NONE, root, convert(e));
+		}
+		
+		return root;
+	}
+
+	public static boolean canShow(Type type) {
+		return true;
+	}
+
+    private GraphNode getOrCreateNode(IValue value, String label) {
+    	GraphNode node;
+    	
+        if (fNodeCache.containsKey(value)) {
+            node = fNodeCache.get(value);
+        } 
+        else {
+            node = new GraphNode(graph, SWT.NONE, label);
+            fNodeCache.put(value, node);
         }
 
         return node;
     }
-    
-	private String getNodeName(IValue value) {
-		return value.toString();
-	}
 }
