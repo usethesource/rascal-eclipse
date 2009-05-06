01bbdca51180a9fbffb8370c28e213dd0900e583
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-05-06 08:39:43 +0000

    -Added extension point so you can set the library path. -Modified dependencies (depends on rascal-plugin now).

--- src/org/meta_environment/rascal/eclipse/Activator.java
+++ src/org/meta_environment/rascal/eclipse/Activator.java
@@ -1,13 +1,47 @@
 package org.meta_environment.rascal.eclipse;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.imp.runtime.PluginBase;
+import org.osgi.framework.Bundle;
+
+import sglr.SGLRInvoker;
 
 public class Activator extends PluginBase {
-	public static final String kPluginID = "rascal_eclipse";
+	public static final String PLUGIN_ID = "rascal_eclipse";
 	public static final String kLanguageName = "Rascal";
+	
+	private final static String RASCAL_BASE_LIBRARY_PATH = "baseLibraryPath";
+	
+	public Activator(){
+		super();
+		
+		Activator.getInstance(); // Stupid ...
+	}
 
 	private static class InstanceKeeper {
-		static Activator sInstance = new Activator();
+		private final static Activator sInstance = new Activator();
+		static{
+			IExtensionRegistry registry = Platform.getExtensionRegistry();
+			IExtensionPoint point = registry.getExtensionPoint(PLUGIN_ID, RASCAL_BASE_LIBRARY_PATH);
+			
+			IExtension extensions[] = point.getExtensions();
+			if(extensions.length > 0){
+				IConfigurationElement[] binaryProviderElements = extensions[0].getConfigurationElements();
+				IConfigurationElement ce = binaryProviderElements[0];
+				String bundle = ce.getAttribute("bundle");
+				String path = ce.getAttribute("path");
+				
+				String baseLibraryPath = getFile(Platform.getBundle(bundle), path);
+				
+				SGLRInvoker.setBaseLibraryPath(baseLibraryPath); // Set the base library path
+			}
+		}
 	}
 
 	public static Activator getInstance() {
@@ -15,7 +49,7 @@ public class Activator extends PluginBase {
 	}
 
 	public String getID() {
-		return kPluginID;
+		return PLUGIN_ID;
 	}
 
 	@Override
@@ -66,5 +100,18 @@ public class Activator extends PluginBase {
 	}
 
 	// Definitions for image management end
-
+	
+	private static String getFile(Bundle bundle, String path){
+		String fileURL;
+		try{
+			fileURL = FileLocator.toFileURL(bundle.getEntry(path)).getPath();
+		}catch(Exception ex){
+			try{
+				fileURL = FileLocator.toFileURL(bundle.getResource(path)).getPath();
+			}catch(Exception ex2){
+				fileURL = FileLocator.find(bundle, new Path(path), null).getPath();
+			}
+		}
+		return fileURL;
+	}
 }
