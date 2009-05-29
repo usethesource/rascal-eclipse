5d11af6f3994d3588e4868c49aa837f79716effe
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-05-29 15:29:44 +0000

    beginnings of a project nature

---
+++ src/org/meta_environment/rascal/eclipse/nature/Nature.java
@@ -0,0 +1,27 @@
+package org.meta_environment.rascal.eclipse.nature;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class Nature implements IProjectNature {
+	private IProject project;
+
+	public void configure() throws CoreException {
+
+	}
+
+	public void deconfigure() throws CoreException {
+
+	}
+
+	public IProject getProject() {
+		return project;
+	}
+
+	public void setProject(IProject project) {
+		this.project = project;
+
+	}
+
+}
