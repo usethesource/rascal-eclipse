83da6aa0445f8f079c5f53f374c00f0f3a723a4f
Emilie Balland <emilie.balland@gmail.com>
2009-08-06 13:06:32 +0000

    * use straight splines for zest layouts

--- src/org/dancingbear/graphbrowser/layout/zest/ZestLayout.java
+++ src/org/dancingbear/graphbrowser/layout/zest/ZestLayout.java
@@ -3,8 +3,12 @@ package org.dancingbear.graphbrowser.layout.zest;
 import java.util.List;
 
 import org.dancingbear.graphbrowser.layout.Layout;
+import org.dancingbear.graphbrowser.layout.dot.FullEdge;
+import org.dancingbear.graphbrowser.layout.dot.PointDouble;
+import org.dancingbear.graphbrowser.layout.model.CubicBezierCurve;
 import org.dancingbear.graphbrowser.layout.model.DirectedGraph;
 import org.dancingbear.graphbrowser.layout.model.Edge;
+import org.dancingbear.graphbrowser.layout.model.Spline;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.zest.layouts.InvalidLayoutConfiguration;
 import org.eclipse.zest.layouts.LayoutEntity;
@@ -32,7 +36,8 @@ public class ZestLayout implements Layout {
 		relationships.toArray(layoutRelationships);
 
 		try {
-			algorithm.applyLayout(layoutEntities, layoutRelationships, 0, 0, graph.getLayoutSize().height, graph.getLayoutSize().width, false, false);
+			//TODO: find a way to determine the optimal size for the graph
+			algorithm.applyLayout(layoutEntities, layoutRelationships, 0, 0, graph.getLayoutSize().height * 3, graph.getLayoutSize().width * 3, false, false);
 		} catch (InvalidLayoutConfiguration e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -41,12 +46,22 @@ public class ZestLayout implements Layout {
 		//it seems that the algorithm only updates the nodes but not the points of the edge
 		//we need an extra phase to update the edges
 		for (Edge e: graph.getEdges()) {
+
 			PointList pts = new PointList();
 			pts.addPoint(e.getSource().getX(), e.getSource().getY());
 			pts.addPoint(e.getTarget().getX(),  e.getTarget().getY());
 			e.setPoints(pts);
-			// no spline defined for this edge so the translation will draw a straight line
-			e.setSpline(null);
+
+			//construct a straight spline
+			FullEdge fullEdge = new FullEdge(e, graph);
+			PointDouble start = fullEdge.getEdgeStartPosition();
+			PointDouble end = fullEdge.getEdgeEndPosition();
+			double endScale = 1.0 / 3.0;
+			double startScale = 1.0 - endScale;
+			PointDouble control1 = start.scale(startScale).add(end.scale(endScale));
+			PointDouble control2 = end.scale(startScale).add(start.scale(endScale));
+			e.setSpline(new Spline(new CubicBezierCurve(start, control1, control2, end)));
+
 		}
 
 	}
