30c5e5bb4517e2981cef4160ac0d6727fa7eb9a6
Emilie Balland <emilie.balland@gmail.com>
2009-08-25 15:03:50 +0000

    * fix the source path lookup for the debugging of a specific Rascal file (lookup at the Eclipse project containing the file and at all its referenced projects)

--- src/org/meta_environment/rascal/eclipse/debug/core/sourcelookup/RascalSourcePathComputerDelegate.java
+++ src/org/meta_environment/rascal/eclipse/debug/core/sourcelookup/RascalSourcePathComputerDelegate.java
@@ -1,17 +1,25 @@
 package org.meta_environment.rascal.eclipse.debug.core.sourcelookup;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.sourcelookup.ISourceContainer;
 import org.eclipse.debug.core.sourcelookup.ISourcePathComputerDelegate;
 import org.eclipse.debug.core.sourcelookup.containers.ProjectSourceContainer;
+import org.meta_environment.rascal.eclipse.IRascalResources;
 
 public class RascalSourcePathComputerDelegate implements ISourcePathComputerDelegate{
 
@@ -20,27 +28,38 @@ public class RascalSourcePathComputerDelegate implements ISourcePathComputerDele
 	 */
 	public ISourceContainer[] computeSourceContainers(ILaunchConfiguration configuration, IProgressMonitor monitor) throws CoreException {
 
-		List<ISourceContainer> containers = new ArrayList<ISourceContainer>();
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		for (IProject project : root.getProjects()) {
-			if (project.isOpen()) {
-				containers.add(new ProjectSourceContainer(project, false));
+		String mainModule = configuration.getAttribute(IRascalResources.ATTR_RASCAL_PROGRAM, (String)null);
+		String mainProject = configuration.getAttribute(IRascalResources.ATTR_RASCAL_PROJECT, (String)null);
+
+		LinkedList<ISourceContainer> sourceContainers = new  LinkedList<ISourceContainer>();
+
+		if (mainProject != null) {
+			//when a project is specified, lookup in the project and its referenced projects
+			LinkedList<IProject> projects = new LinkedList<IProject>();
+			projects.add(ResourcesPlugin.getWorkspace().getRoot().getProject(mainProject));
+
+			while (! projects.isEmpty()) {
+				IProject project = projects.removeFirst();
+				sourceContainers.add(new ProjectSourceContainer(project, false));
+				projects.addAll(Arrays.asList(project.getDescription().getReferencedProjects()));
 			}
-		}
-		return containers.toArray( new ISourceContainer[]{});
+			return sourceContainers.toArray(new ISourceContainer[]{});
+		}	
 
-		/**
-		 //this code will be used when debugging a specific file
-		String path = configuration.getAttribute(IRascalResources.ATTR_RASCAL_PROGRAM, (String)null);
-		ISourceContainer sourceContainer = null;
-		if (path != null) {
-			IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(path));
-				IProject project = resource.getProject();
-				sourceContainer = new ProjectSourceContainer(project, false);
-				return new ISourceContainer[]{sourceContainer};
-		}
-	}
-		 */
+		if (mainModule != null) {
+			//when a specific file is specified, lookup in its corresponding project (+ referenced projects)
+			//when a project is specified, lookup in the project and its referenced projects
+			LinkedList<IProject> projects = new LinkedList<IProject>();
+			projects.add(ResourcesPlugin.getWorkspace().getRoot().findMember(mainModule).getProject());
 
+			while (! projects.isEmpty()) {
+				IProject project = projects.removeFirst();
+				sourceContainers.add(new ProjectSourceContainer(project, false));
+				projects.addAll(Arrays.asList(project.getDescription().getReferencedProjects()));
+			}
+			return sourceContainers.toArray(new ISourceContainer[]{});
+			}
+		/* default case */
+		return new ISourceContainer[]{};
 	}
 }
