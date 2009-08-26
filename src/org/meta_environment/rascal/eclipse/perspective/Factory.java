88637d266013d94bac20ec7cddbcea54f9dea21b
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-08-26 11:12:10 +0000

    -Removed unnecessary import.

--- src/org/meta_environment/rascal/eclipse/perspective/Factory.java
+++ src/org/meta_environment/rascal/eclipse/perspective/Factory.java
@@ -5,7 +5,6 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eclipse.ui.console.IConsoleConstants;
-import org.meta_environment.rascal.eclipse.console.ConsoleFactory;
 
 public class Factory implements IPerspectiveFactory {
 
