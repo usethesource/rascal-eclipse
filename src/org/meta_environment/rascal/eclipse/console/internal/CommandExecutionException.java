cf77052b7f1c0a66bfabdf1ac01062f9e4ecedf7
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-09-30 09:45:01 +0000

    -Added 'default' constructor which does not set the offset.

--- src/org/meta_environment/rascal/eclipse/console/internal/CommandExecutionException.java
+++ src/org/meta_environment/rascal/eclipse/console/internal/CommandExecutionException.java
@@ -1,9 +1,15 @@
 package org.meta_environment.rascal.eclipse.console.internal;
 
 public class CommandExecutionException extends Exception{
-	private static final long serialVersionUID = -4226914269976638377L;
+	private static final long serialVersionUID = -7113571508827328424L;
 	
 	private final int offset;
+	
+	public CommandExecutionException(String message){
+		super(message);
+		
+		this.offset = -1;
+	}
 	
 	public CommandExecutionException(String message, int offset){
 		super(message);
