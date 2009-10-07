b4e61e9ec87b79322b0901133eaeb1612f5b8f48
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-10-07 14:05:10 +0000

    trying to add a short cut

--- src/org/meta_environment/rascal/eclipse/perspective/actions/CopyToConsole.java
+++ src/org/meta_environment/rascal/eclipse/perspective/actions/CopyToConsole.java
@@ -1,5 +1,8 @@
 package org.meta_environment.rascal.eclipse.perspective.actions;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -12,16 +15,23 @@ import org.meta_environment.rascal.eclipse.Activator;
 import org.meta_environment.rascal.eclipse.console.ConsoleFactory;
 import org.meta_environment.rascal.eclipse.console.ConsoleFactory.IRascalConsole;
 
-public class CopyToConsole implements IEditorActionDelegate {
+public class CopyToConsole extends AbstractHandler implements IEditorActionDelegate {
 	private UniversalEditor editor;
 
 	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
 		if (targetEditor instanceof UniversalEditor) {
 			this.editor = (UniversalEditor) targetEditor;
 		}
+		else {
+			this.editor = null;
+		}
 	}
 	
 	public void run(IAction action) {
+		if (editor == null) {
+			return;
+		}
+		
 		String cmd = editor.getSelectionText();
 		IConsoleManager man = ConsolePlugin.getDefault().getConsoleManager();
 		
@@ -42,5 +52,10 @@ public class CopyToConsole implements IEditorActionDelegate {
 	}
 	
 	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		run(null);
+		return null;
 	}
 }
