7267ea47560c9dce707be20681fa2168bb9aabbf
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2009-10-20 14:41:53 +0000

    org imports

--- src/org/meta_environment/rascal/eclipse/library/viz/Basic.java
+++ src/org/meta_environment/rascal/eclipse/library/viz/Basic.java
@@ -1,16 +1,7 @@
 package org.meta_environment.rascal.eclipse.library.viz;
 
-import org.eclipse.imp.pdb.facts.IInteger;
-import org.eclipse.imp.pdb.facts.IList;
-import org.eclipse.imp.pdb.facts.IMap;
-import org.eclipse.imp.pdb.facts.IString;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.ui.graph.Editor;
-import org.meta_environment.rascal.library.viz.BarChart;
-import org.meta_environment.rascal.library.viz.BoxPlot;
-import org.meta_environment.rascal.library.viz.Histogram;
-import org.meta_environment.rascal.library.viz.PieChart;
-import org.meta_environment.rascal.library.viz.XYChart;
 
 public class Basic {
 	
