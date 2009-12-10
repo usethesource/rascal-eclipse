305ab5d26d728d9454954081d918f8398f278a3e
Arnold Lankamp <arnold.lankamp@gmail.com>
2009-12-10 14:26:22 +0000

    -Added something which enables the user to run 'X' commands, for 'Y' projects (1 console per project).
    Here is an example of the usage:
    
    import scripting::Scripting;
    execute(["import IO;", "println(\"test\");"], [|project://bla|, |project://argh|]);

---
+++ src/org/meta_environment/rascal/eclipse/library/scripting/CommandScripter.java
@@ -0,0 +1,46 @@
+package org.meta_environment.rascal.eclipse.library.scripting;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.ISourceLocation;
+import org.eclipse.imp.pdb.facts.IString;
+import org.eclipse.imp.pdb.facts.IValueFactory;
+import org.meta_environment.rascal.eclipse.console.ConsoleFactory;
+import org.meta_environment.rascal.eclipse.console.ConsoleFactory.IRascalConsole;
+
+public class CommandScripter{
+	private static final IWorkspaceRoot WORKSPACE = ResourcesPlugin.getWorkspace().getRoot();
+	
+	public CommandScripter(IValueFactory valueFactory){
+		super();
+	}
+	
+	public void execute(IList commandsList, IList projectsList){
+		String[] commands = new String[commandsList.length()];
+		for(int i = commandsList.length() - 1; i >= 0; i--){
+			commands[i] = ((IString) commandsList.get(i)).getValue();
+		}
+		
+		IProject[] projects = new IProject[projectsList.length()];
+		for(int i = projectsList.length() - 1; i >= 0; i--){
+			projects[i] = WORKSPACE.getProject(((ISourceLocation) projectsList.get(i)).getURI().getHost());
+		}
+		
+		ConsoleFactory cf = ConsoleFactory.getInstance();
+		
+		for(int i = 0 ; i < projects.length; i++){
+			IProject project = projects[i];
+			IRascalConsole console = cf.openRunOutputConsole(project);
+			try{
+				for(int j = 0; j < commands.length; j++){
+					console.executeCommand(commands[j]+"\n");
+				}
+			}catch(RuntimeException rex){
+				System.err.println("Failed to execute commands in project: "+project+", cause: "+rex.getMessage());
+			}
+			//console.terminate();
+		}
+	}
+}
---
+++ src/org/meta_environment/rascal/eclipse/library/scripting/Scripting.rsc
@@ -0,0 +1,4 @@
+module scripting::Scripting
+
+@javaClass{org.meta_environment.rascal.eclipse.library.scripting.CommandScripter}
+public void java execute(list[str] commands, list[loc] projects);
