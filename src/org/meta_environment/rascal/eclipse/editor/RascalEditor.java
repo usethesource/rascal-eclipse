1c4a41c1f12dd8ca280decbcc46a6106a06bdfcc
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-01-05 09:47:21 +0000

    -Fixed "F2 focus error bug".

--- src/org/meta_environment/rascal/eclipse/editor/RascalEditor.java
+++ src/org/meta_environment/rascal/eclipse/editor/RascalEditor.java
@@ -1,6 +1,10 @@
 package org.meta_environment.rascal.eclipse.editor;
 
 import org.eclipse.imp.editor.UniversalEditor;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.texteditor.ITextEditorActionConstants;
+import org.eclipse.ui.texteditor.ITextEditorActionDefinitionIds;
 
 public class RascalEditor extends UniversalEditor {
 	
@@ -8,5 +12,14 @@ public class RascalEditor extends UniversalEditor {
         super();
         // used for the expression breakpoints management
         setEditorContextMenuId("rascal.editor.contextMenu");
+    }
+    
+    protected void createActions() {
+    	super.createActions();
+    	
+    	IAction action = new Action(){/* Nothing. */};
+    	action.setActionDefinitionId(ITextEditorActionDefinitionIds.SHOW_INFORMATION);
+        
+        setAction(ITextEditorActionConstants.SHOW_INFORMATION, action);
     }
 }
