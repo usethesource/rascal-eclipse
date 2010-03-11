b0cbc63f3ee2192f531cd9b4a6a14618b106d076
Bert Lisser <Bert.Lisser@cwi.nl>
2010-03-11 16:05:26 +0000

    IFile file.getLocal() replaced by file.getLocalUri(), so that .rsc files outside the eclipse workSpace are reachable.

--- src/org/rascalmpl/eclipse/box/MakeBox.java
+++ src/org/rascalmpl/eclipse/box/MakeBox.java
@@ -11,7 +11,15 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdapterManager;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.imp.pdb.facts.IConstructor;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.io.PBFWriter;
@@ -33,6 +41,7 @@ import org.eclipse.ui.part.FileEditorInput;
 import org.rascalmpl.ast.ASTFactory;
 import org.rascalmpl.ast.Command;
 import org.rascalmpl.ast.Module;
+import org.rascalmpl.eclipse.IRascalResources;
 import org.rascalmpl.eclipse.console.ConsoleFactory;
 import org.rascalmpl.interpreter.BoxEvaluator;
 import org.rascalmpl.interpreter.env.ModuleEnvironment;
@@ -62,50 +71,89 @@ public class MakeBox implements IObjectActionDelegate, IActionDelegate2,
 	public void runWithEvent(IAction action, Event event) {
 		run(action);
 	}
+	
+private void launch(String path, IValue v) {
+	String mode="run";
+	// System.err.println("launch:"+path);
+    ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+    ILaunchConfigurationType type = launchManager.getLaunchConfigurationType(IRascalResources.ID_RASCAL_LAUNCH_CONFIGURATION_TYPE);
+    try {
+        ILaunchConfiguration[] configurations = launchManager.getLaunchConfigurations(type);
+        for (int i = 0; i < configurations.length; i++) {
+            ILaunchConfiguration configuration = configurations[i];
+            String attribute = configuration.getAttribute(IRascalResources.ATTR_RASCAL_PROGRAM, (String)null);
+            // System.err.println("attribute:"+attribute);
+            if (path.equals(attribute)) {
+                DebugUITools.launch(configuration, mode);
+                return;
+            }
+        }
+    } catch (CoreException e) {
+        return;
+    }
+    
+    try {
+        // create a new configuration for the rascal file
+        ILaunchConfigurationWorkingCopy workingCopy = type.newInstance(null, file.getName());
+        workingCopy.setAttribute(IRascalResources.ATTR_RASCAL_PROGRAM, path);
+        ILaunchConfiguration configuration = workingCopy.doSave();
+        DebugUITools.launch(configuration, mode);
+    } catch (CoreException e1) {
+    }
+}
 
 	public void run(IAction action) {
-		IPath path = file.getLocation();
-		// System.err.println("path:"+path.toString());
+		URI uri = file.getLocationURI();
+//	    String name = file.getName();
+//		name = name.substring(0,name.lastIndexOf('.'));
+		final String name = "tst";
 		// register some schemes
-		URIResolverRegistry registry = URIResolverRegistry.getInstance();
-		FileURIResolver files = new FileURIResolver();
-		registry.registerInput(files.scheme(), files);
-		URI uri = FileURIResolver.constructFileURI(path.toString());
-		// System.err.println("uri:"+uri);
+		// URIResolverRegistry registry = URIResolverRegistry.getInstance();
+		// FileURIResolver files = new FileURIResolver();
+		// registry.registerInput(files.scheme(), files);
+		// uri = FileURIResolver.constructFileURI(uri.getPath());
 		ModuleEnvironment root = new ModuleEnvironment("***test***");
 		try {
 			IConstructor tree = loader.parseModule(uri, root);
 			Module module = new ASTBuilder(new ASTFactory()).buildModule(tree);
 			TypeStore ts = ((BoxEvaluator) eval).getTypeStore();
+			System.err.println("Checked");
 			IValue v = eval.evalRascalModule(module);
-			Display display = Display.getCurrent();
-			Shell shell = new Shell (display);
-			shell.open ();
-			FileDialog dialog = new FileDialog (shell, SWT.SAVE);
-			String [] filterExtensions = new String [] {"*.box"};
-			dialog.setFilterExtensions (filterExtensions);
-			dialog.setFileName ("tst.box");
-			String file = dialog.open ();
-			System.out.println ("Save to: " + file);
-			shell.close();
-			while (!shell.isDisposed ()) {
-				if (!display.readAndDispatch ()) display.sleep ();
-			}	
-			display.dispose ();
+//			Display display = Display.getCurrent();
+//			Shell shell = new Shell (display);
+//			shell.open ();
+//			FileDialog dialog = new FileDialog (shell, SWT.SAVE);
+//			String [] filterExtensions = new String [] {"*.box"};
+//			dialog.setFilterExtensions (filterExtensions);
+//			dialog.setFileName (name+".box");
+//			String file = dialog.open ();
+//			dialog.getParent().close();
+//			if (file==null) {
+//				System.err.println("Canceled");
+//				return;
+//			}
+//			System.out.println ("Save to: " + file);
+//			while (!shell.isDisposed ()) {
+//				if (!display.readAndDispatch ()) display.sleep ();
+//			}	
+//			display.dispose ();
 			PBFWriter.writeValueToFile(v,
-					new File(file), ts);
-			// System.err.println("MODULE:" + v);
-			// System.err.println(stat.getHeader().toString());
+					new File(name+".box"), ts);
+//			// System.err.println("MODULE:" + v);
+//			System.err.println("Launch /box/src/Box2Text.rsc");
+			
+			launch("/box/src/Box2Text.rsc", v);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}	
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
 		if (selection instanceof IStructuredSelection) {
 			IStructuredSelection ss = (IStructuredSelection) selection;
 			Object element = ss.getFirstElement();
+			System.err.println("Selected:"+element+" "+element.getClass());
 			if (element instanceof IProject) {
 				project = (IProject) element;
 			} else if (element instanceof IFolder) {
