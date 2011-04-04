880964a3ec5f170b72ae3bac82de7e7031420e83
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2011-04-04 15:30:20 +0000

    * added license banners, using svn log info to retrieve the contributors of each file.

--- src/org/rascalmpl/eclipse/library/lang/java/jdt/internal/Java.java
+++ src/org/rascalmpl/eclipse/library/lang/java/jdt/internal/Java.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2011 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   * 
+ *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
+ *   * Bas Basten - Bas.Basten@cwi.nl (CWI)
+ *   * Jouke Stoel - Jouke.Stoel@cwi.nl (CWI)
+ *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
+*******************************************************************************/
 package org.rascalmpl.eclipse.library.lang.java.jdt.internal;
 
 import org.eclipse.imp.pdb.facts.type.Type;
