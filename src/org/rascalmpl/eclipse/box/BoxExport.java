ea70fadee4d27e95ff719dfc10a05345716fc928
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2011-04-06 11:53:49 +0000

    added banners

--- src/org/rascalmpl/eclipse/box/BoxExport.java
+++ src/org/rascalmpl/eclipse/box/BoxExport.java
@@ -1,4 +1,16 @@
 /*******************************************************************************
+ * Copyright (c) 2009-2011 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
+ *   * Bert Lisser - Bert.Lisser@cwi.nl (CWI)
+ *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
+*******************************************************************************/
+/*******************************************************************************
  * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
--- src/org/rascalmpl/eclipse/library/lang/java/jdt/Java.rsc
+++ src/org/rascalmpl/eclipse/library/lang/java/jdt/Java.rsc
@@ -1,3 +1,16 @@
+@license{
+  Copyright (c) 2009-2011 CWI
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of the Eclipse Public License v1.0
+  which accompanies this distribution, and is available at
+  http://www.eclipse.org/legal/epl-v10.html
+}
+@contributor{}
+@contributor{Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI}
+@contributor{Bas Basten - Bas.Basten@cwi.nl (CWI)}
+@contributor{Jouke Stoel - Jouke.Stoel@cwi.nl (CWI)}
+@contributor{Mark Hills - Mark.Hills@cwi.nl (CWI)}
+@contributor{Arnold Lankamp - Arnold.Lankamp@cwi.nl}
 module lang::java::jdt::Java
 
 import List;
