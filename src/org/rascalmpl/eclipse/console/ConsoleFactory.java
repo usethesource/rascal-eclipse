3044a25f35a3f060ca520dfec01e5a2b9e48a8d9
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2011-05-05 07:51:25 +0000

    put Emilie's affiliation to CWI

--- src/org/rascalmpl/eclipse/console/ConsoleFactory.java
+++ src/org/rascalmpl/eclipse/console/ConsoleFactory.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
  *   * Tijs van der Storm - Tijs.van.der.Storm@cwi.nl
- *   * Emilie Balland - emilie.balland@inria.fr (INRIA)
+ *   * Emilie Balland - (CWI)
  *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
 *******************************************************************************/
 package org.rascalmpl.eclipse.console;
