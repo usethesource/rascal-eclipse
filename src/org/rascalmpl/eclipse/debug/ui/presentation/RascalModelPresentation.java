559c111359a2f787f4ce5ac48c07be518a69c4ae
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2011-05-05 07:48:15 +0000

    fixed headers

--- src/org/rascalmpl/eclipse/debug/ui/presentation/RascalModelPresentation.java
+++ src/org/rascalmpl/eclipse/debug/ui/presentation/RascalModelPresentation.java
@@ -7,20 +7,9 @@
  *
  * Contributors:
  *   * Jurgen J. Vinju - Jurgen.Vinju@cwi.nl - CWI
- *   * Emilie Balland - emilie.balland@inria.fr (INRIA)
+ *   * Emilie Balland - emilie.balland@cwi.nl (CWI)
  *   * Arnold Lankamp - Arnold.Lankamp@cwi.nl
 *******************************************************************************/
-/*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Bjorn Freeman-Benson - initial API and implementation
- *******************************************************************************/
 package org.rascalmpl.eclipse.debug.ui.presentation;
 
 import org.eclipse.core.resources.IFile;
