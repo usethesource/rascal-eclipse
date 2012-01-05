dda52916f5012d66a0c26f95e22d857afca52eb0
Davy Landman <davy.landman@cwi.nl>
2012-01-05 19:16:41 +0000

    Re-implemented the ConcurrentCircularOutputStream: a multiple writers single reader console
    Sadly enough I managed to make the writers lock-free, but I need to lock them out when you want to read from the OutputStream..
    
    But it is very fast and both interleaving and correct sequence on write level is guaranteed.

--- src/org/rascalmpl/eclipse/console/internal/ConcurrentCircularOutputStream.java
+++ src/org/rascalmpl/eclipse/console/internal/ConcurrentCircularOutputStream.java
@@ -3,14 +3,33 @@ package org.rascalmpl.eclipse.console.internal;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 
+/**
+ * This OutputStream allows multiple threads to write to it without requiring any locking.
+ * The retrieving (getBufferCopy) of the data in the stream however locks internally and you should avoid calling it
+ * from other threads.
+ * 
+ * To avoid possible deadlocks we advise to  call getBufferCopy in a separate worker (outside of the UI-thread).
+ * 
+ * @author Davy Landman 
+ *
+ */
 public class ConcurrentCircularOutputStream extends OutputStream {
 	private final byte[] buffer;
 	private final int bufferSize;
 	private final int bufferMod;
 	private final AtomicInteger bytesWritten;
-	
+	private final ReadWriteLock resultRWLock; // we are actually using it reverse, multiple writers single reader..
+	private boolean isDirty; 
+
+	/**
+	 * Create a new ConcurrentCircularOutputStream which allows for fast writing from multiple threads.
+	 * Correct order and interleaving between write()'s are guaranteed.
+	 * @param bufferSize the size of the circular buffer, due to implementation details this size must be a power of 2!
+	 */
 	public ConcurrentCircularOutputStream(int bufferSize) {
 		assert bufferSize > 0;
 		if ((bufferSize & (bufferSize - 1)) != 0) {
@@ -20,13 +39,22 @@ public class ConcurrentCircularOutputStream extends OutputStream {
 		this.bufferMod = bufferSize - 1;
 		buffer = new byte[this.bufferSize];
 		bytesWritten = new AtomicInteger(0);
+		resultRWLock = new ReentrantReadWriteLock(true);
+		isDirty = false;
 	}
 	
 	@Override
 	public void write(int b) throws IOException {
-		int endPos = bytesWritten.incrementAndGet();
-		int writePos =(endPos - 1) & bufferMod;
-		buffer[writePos] = (byte)b;
+		resultRWLock.readLock().lock();
+		try {
+			isDirty = true;
+			int endPos = bytesWritten.incrementAndGet();
+			int writePos =(endPos - 1) & bufferMod;
+			buffer[writePos] = (byte)b;
+		}
+		finally {
+			resultRWLock.readLock().unlock();
+		}
 	}
 
 	@Override
@@ -45,80 +73,72 @@ public class ConcurrentCircularOutputStream extends OutputStream {
 			off = off + (len - bufferSize);
 			len = bufferSize;
 		}
-		writeChecked(b, off, len);
+		
+		resultRWLock.readLock().lock();
+		try {
+			isDirty = true;
+			int endPos = bytesWritten.addAndGet(len);
+			int startPos = (endPos - len) & bufferMod;
+			
+			if (startPos + len <= bufferSize) {
+				// go ahead and write!
+				System.arraycopy(b, off, buffer, startPos, len);
+			} 
+			else {
+				// we have to write over the edge of the buffer. so be smarter here!
+				int firstChunkLength = bufferSize - startPos;
+				System.arraycopy(b, off, buffer, startPos, firstChunkLength);
+				// now we have to go back to the front of the buffer
+				int secondChunkLength = len - firstChunkLength;
+				System.arraycopy(b, off + firstChunkLength, buffer, 0, secondChunkLength);
+			}
+		}
+		finally {
+			resultRWLock.readLock().unlock();
+		}
 	}
 	
-	private void writeChecked(byte[] b, int off, int len) {
-		assert len <= bufferSize;
-		
-		int endPos = bytesWritten.addAndGet(len);
-		int startPos = (endPos - len) & bufferMod;
-		
-		if (startPos + len <= bufferSize) {
-			// go ahead and write!
-			System.arraycopy(b, off, buffer, startPos, len);
-		} 
-		else {
-			// we have to write over the edge of the buffer. so be smarter here!
-			int firstChunkLength = bufferSize - startPos;
-			System.arraycopy(b, off, buffer, startPos, firstChunkLength);
-			// now we have to go back to the front of the buffer
-			int secondChunkLength = len - firstChunkLength;
-			System.arraycopy(b, off + firstChunkLength, buffer, 0, secondChunkLength);
-		}
-	}
-	
-	/**
-	 * You should only call this from 1 thread!
-	 * @return
-	 */
-	public byte[] catchUpToBuffer() {
-		/*
-		// we try to get current state 
-		int bufferOffset = bufferWritePointer.get();
-		int totalBytesWritten = bytesWritten.get();
-		// since the bytesWritten and the bufferOffset are changed independently from each other (no double CAS operations)
-		// we need to make sure they align
-		while (bufferOffset != bufferWritePointer.get() || bufferOffset > bufferSize) {
-			bufferOffset = bufferWritePointer.get();
-			totalBytesWritten = bytesWritten.get();
-			// we are looking for a moment where they remain stable
-			// such that we are sure we can read x bytes back from the bufferOffset position
-		}
-		if (totalBytesWritten == 0) { 
-			return new byte[0];
-		}
-		resetBytesWrittenCounter(totalBytesWritten);
-		
-		int bytesToCatchUp = Math.min(totalBytesWritten, bufferSize);
-		byte[] result = new byte[bytesToCatchUp];
-		
-		if (bufferOffset - bytesToCatchUp >= 0) {
-			//System.err.printf("buffoffset: %d bytecatch: %d\n", bufferOffset, bytesToCatchUp);
-			System.arraycopy(buffer, bufferOffset - bytesToCatchUp, result, 0, bytesToCatchUp);
-		}
-		else {
-			// we have to copy twice because we loop around to the end of the buffer
-			int firstChunkLength = bufferOffset;// first chunk is the last part of the array!
-			int secondChunkLength = bytesToCatchUp - firstChunkLength;
-			System.arraycopy(buffer, bufferSize - secondChunkLength, result, 0, secondChunkLength);
-			System.arraycopy(buffer, 0, result, secondChunkLength, firstChunkLength);
-		}
-		return result;
-		*/
-		return new byte[0];
+	public boolean getIsDirty() {
+		return isDirty;
 	}
 
-	private void resetBytesWrittenCounter(int totalBytesWritten) {
-		int newByteWrittenCounterValue = 0;
-		int oldByteWrittenCounterValue = totalBytesWritten;
-		// first let's update the byteWritten counter
-		while (!bytesWritten.compareAndSet(oldByteWrittenCounterValue, newByteWrittenCounterValue)) {
-			// if we could not update, new bytes were already written
-			// so we have to initialize the bytesWritten counter at a different number than 0
-			oldByteWrittenCounterValue = bytesWritten.get();
-			newByteWrittenCounterValue = Math.max(0, oldByteWrittenCounterValue - totalBytesWritten);
+	/**
+	 * This method creates a copy of what has been written in the buffer since the last call to this method.
+	 * If this was more than the buffer size, it is off course truncated to that limit.
+	 * This call waits for all the writes to finish and than blocks the writes until it is finished, therefore 
+	 * be careful with the calling frequency.
+	 * 
+	 * @return a copy of the bytes in the buffer.
+	 */
+	public byte[] getBufferCopy() {
+		resultRWLock.writeLock().lock();
+		try {
+			int totalWritten = bytesWritten.get();
+			byte[] result;
+			if (totalWritten > 0 && totalWritten < bufferSize) {
+				// not run outside of the first buffer yet
+				result = new byte[totalWritten];
+				System.arraycopy(buffer, 0, result, 0, totalWritten);
+			}
+			else if (isDirty) {
+				// pos could be 0 but that could be either that no write has happend yet
+				// or that we just returned to 0 after a lot of writes
+				result = new byte[bufferSize];
+				int bufferOffset = totalWritten & bufferMod;
+				int firstChunkLength = bufferOffset;// first chunk is the last part of the array!
+				int secondChunkLength = bufferSize - firstChunkLength;
+				System.arraycopy(buffer, bufferSize - secondChunkLength, result, 0, secondChunkLength);
+				System.arraycopy(buffer, 0, result, secondChunkLength, firstChunkLength);
+			}
+			else {
+				result = new byte[0];
+			}
+			bytesWritten.set(0);
+			isDirty = false;
+			return result;
+		}
+		finally {
+			resultRWLock.writeLock().unlock();
 		}
 	}
-	
 }
