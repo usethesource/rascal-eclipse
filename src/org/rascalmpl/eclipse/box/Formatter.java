4689ffa024c20eecf2ccb40b1ad315afe25599c9
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2012-01-21 13:53:04 +0000

    taken the Rascal formatter offline because I've broken it severely by removing access to parse trees in ASTs some weeks ago

--- src/org/rascalmpl/eclipse/box/Formatter.java
+++ src/org/rascalmpl/eclipse/box/Formatter.java
@@ -12,19 +12,11 @@
 package org.rascalmpl.eclipse.box;
 
 import org.eclipse.imp.parser.IParseController;
-import org.eclipse.imp.pdb.facts.IConstructor;
-import org.eclipse.imp.pdb.facts.IList;
 import org.eclipse.imp.services.ISourceFormatter;
-import org.rascalmpl.ast.Module;
-import org.rascalmpl.eclipse.editor.ParseController;
-import org.rascalmpl.interpreter.BoxEvaluator;
-import org.rascalmpl.library.box.MakeBox;
-import org.rascalmpl.parser.ASTBuilder;
-import org.rascalmpl.values.uptr.TreeAdapter;
 
 public class Formatter implements ISourceFormatter{
 	
-	BoxEvaluator eval = new BoxEvaluator();
+//	BoxEvaluator eval = new BoxEvaluator();
 
 	public void formatterStarts(String initialIndentation) {
 		// TODO Auto-generated method stub
@@ -33,19 +25,7 @@ public class Formatter implements ISourceFormatter{
 
 	public String format(IParseController parseController, String content,
 			boolean isLineStart, String indentation, int[] positions) {
-		ParseController p = (ParseController) parseController;
-		MakeBox makeBox = new MakeBox();
-		IConstructor currentAST = (IConstructor) p.getCurrentAst();
-		IList z = TreeAdapter.getArgs(currentAST);
-		ASTBuilder astBuilder = new ASTBuilder();
-		Module moduleAst = astBuilder.buildModule(currentAST);
-		// System.err.println("computeBox: build");
-		if (moduleAst != null) {
-		IConstructor a = 	(IConstructor) eval.evalRascalModule(moduleAst, z);
-		return makeBox.box2String(a);
-		// System.err.println(a);
-		}
-		return content;
+		throw new RuntimeException("The Rascal formatter is temporarily unavailable");
 	}
 
 	public void formatterStops() {
