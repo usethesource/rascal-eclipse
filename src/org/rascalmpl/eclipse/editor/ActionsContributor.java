bfcdc8d1d3ece4eaf7e759e9675f6e946ff050b4
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2012-08-27 23:32:03 +0200

    more cleanup. removed Launch Console action

--- src/org/rascalmpl/eclipse/editor/ActionsContributor.java
+++ src/org/rascalmpl/eclipse/editor/ActionsContributor.java
@@ -12,18 +12,13 @@
 *******************************************************************************/
 package org.rascalmpl.eclipse.editor;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.imp.editor.UniversalEditor;
-import org.eclipse.imp.model.ISourceProject;
 import org.eclipse.imp.services.ILanguageActionsContributor;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IStatusLineManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.rascalmpl.eclipse.perspective.actions.BrowseTree;
 import org.rascalmpl.eclipse.perspective.actions.CopyToConsole;
-import org.rascalmpl.eclipse.perspective.actions.LaunchConsoleAction;
 import org.rascalmpl.eclipse.perspective.actions.ListAmbiguities;
 import org.rascalmpl.eclipse.perspective.actions.ReloadStaticChecker;
 import org.rascalmpl.eclipse.perspective.actions.ResetProjectState;
@@ -56,30 +51,10 @@ public class ActionsContributor implements ILanguageActionsContributor {
 	@Override
 	public void contributeToStatusLine(UniversalEditor editor,
 			IStatusLineManager statusLineManager) {
-		// TODO Auto-generated method stub
-
 	}
 
-	@SuppressWarnings("unused")
 	@Override
 	public void contributeToMenuBar(UniversalEditor editor, IMenuManager menu) {
-		ISourceProject sourceProject = editor.getParseController().getProject();
-
-		if (sourceProject != null) {
-			IProject rawProject = sourceProject.getRawProject();
-			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(rawProject.getFullPath().append(editor.getParseController().getPath()));
-
-//			IMenuManager rascalMenu = menu.findMenuUsingPath("rascal");
-//			if (rascalMenu == null) {
-//				rascalMenu = new MenuManager("Rascal","rascal");
-//				menu.add(rascalMenu);
-//			}
-//			
-//			rascalMenu.add(new LaunchConsoleAction(rawProject, file));
-//			rascalMenu.add(new RunAmbiDexter(editor, rawProject, file));
-//			rascalMenu.add(new ListAmbiguities(editor, rawProject, file)); 
-//			rascalMenu.add(new CopyToConsole(editor));		
-		}
 	}
 
 }
