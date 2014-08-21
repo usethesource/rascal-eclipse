4fe27a65171d3fd01442616a84935785065d243b
Jouke Stoel <j.h.stoel@gmail.com>
2014-08-21 16:02:14 +0200

    [MINOR] Added an implementation of a QuickFixAssistent for Terms. If the ParseTree annotated 'messages' contain 'quick fixes' they will be displayed by this class. Also, when selected in the IDE the quick fix function will be applied to replace the source code of the quick fixed file

---
+++ src/org/rascalmpl/eclipse/terms/TermQuickFixAssistant.java
@@ -0,0 +1,133 @@
+package org.rascalmpl.eclipse.terms;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.imp.editor.hover.ProblemLocation;
+import org.eclipse.imp.pdb.facts.IAnnotatable;
+import org.eclipse.imp.pdb.facts.IConstructor;
+import org.eclipse.imp.pdb.facts.IList;
+import org.eclipse.imp.pdb.facts.ISet;
+import org.eclipse.imp.pdb.facts.ISourceLocation;
+import org.eclipse.imp.pdb.facts.IString;
+import org.eclipse.imp.pdb.facts.ITuple;
+import org.eclipse.imp.pdb.facts.IValue;
+import org.eclipse.imp.pdb.facts.IWithKeywordParameters;
+import org.eclipse.imp.pdb.facts.type.Type;
+import org.eclipse.imp.pdb.facts.type.TypeFactory;
+import org.eclipse.imp.services.IQuickFixAssistant;
+import org.eclipse.imp.services.IQuickFixInvocationContext;
+import org.eclipse.imp.utils.NullMessageHandler;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.rascalmpl.interpreter.result.ICallableValue;
+import org.rascalmpl.values.uptr.Factory;
+
+public class TermQuickFixAssistant implements IQuickFixAssistant {
+	
+	private static final String KEYWORD_QUICKFIX = "quickFixes";
+	private static final String ANNOTATION_MESSAGES = "messages";
+
+	@Override
+	public boolean canFix(Annotation annotation) {
+		return true;
+	}
+
+	@Override
+	public boolean canAssist(IQuickFixInvocationContext invocationContext) {
+		return true ;
+	}
+
+	@Override
+	public String[] getSupportedMarkerTypes() {
+		return new String[] { IMarker.PROBLEM };
+	}
+
+	private void constructProposals(IConstructor ast, int problemOffset, Collection<ICompletionProposal> proposals) {
+		IAnnotatable<? extends IConstructor> annotatedAst = ast.asAnnotatable();
+		if (!annotatedAst.hasAnnotation(ANNOTATION_MESSAGES)) {
+			return;
+		}
+		
+		ISet annotations = (ISet) annotatedAst.getAnnotation(ANNOTATION_MESSAGES);
+		
+		for (IValue annotation : annotations) {
+			IConstructor cons = (IConstructor) annotation;
+			ISourceLocation fixLoc = (ISourceLocation)cons.get("at");
+			
+			if (isProblemLocInQuickFixLoc(problemOffset, fixLoc)) {
+				IWithKeywordParameters<IConstructor> withKeywords = cons.asWithKeywordParameters();
+			
+				if (withKeywords.hasParameter(KEYWORD_QUICKFIX)) {
+					IList quickFixes = (IList) withKeywords.getParameter(KEYWORD_QUICKFIX);
+					
+					for (IValue qf: quickFixes) {
+						ITuple quickFix = (ITuple) qf;
+						String label = ((IString)quickFix.get(0)).getValue();
+						ICallableValue function = ((ICallableValue) quickFix.get(1));
+						
+						proposals.add(makeProposal(ast, fixLoc, label, function));
+					}
+				}
+			}
+		}
+	}
+	
+	private boolean isProblemLocInQuickFixLoc(int problemOffset, ISourceLocation quickFixLoc) {
+		return 	quickFixLoc.getOffset() == problemOffset;
+	}
+	
+	@Override
+	public void addProposals(IQuickFixInvocationContext context,
+			ProblemLocation problem, Collection<ICompletionProposal> proposals) {
+		
+		IConstructor ast = (IConstructor)context.getModel().getAST(new NullMessageHandler(), new NullProgressMonitor());
+
+		constructProposals(ast, problem.getOffset(), proposals);
+	}
+
+	private ICompletionProposal makeProposal(final IConstructor ast, final ISourceLocation loc, final String label,
+			final ICallableValue f) {
+		return new ICompletionProposal() {
+			
+			@Override
+			public Point getSelection(IDocument document) {
+				return new Point(loc.getOffset(), 0);
+			}
+			
+			@Override
+			public Image getImage() {
+				return null;
+			}
+			
+			@Override
+			public String getDisplayString() {
+				return label;
+			}
+			
+			@Override
+			public IContextInformation getContextInformation() {
+				return null;
+			}
+			
+			@Override
+			public String getAdditionalProposalInfo() {
+				return null;
+			}
+			
+			@Override
+			public void apply(IDocument document) {
+				Type[] argTypes = new Type[] { Factory.Tree, TypeFactory.getInstance().sourceLocationType() };
+				IString newSrc = (IString)f.call(argTypes, new IValue[] {ast, loc},  Collections.<String,IValue>emptyMap()).getValue();
+				document.set(newSrc.getValue());
+			}
+		};
+	}
+
+}
