0084ef5141e2c8d2cb1ad4cc7815b649254d947f
Jurgen J. Vinju <jurgen@vinju.org>
2015-01-18 18:58:13 +0100

    removed unused imports

--- src/org/rascalmpl/eclipse/console/RascalScriptInterpreter.java
+++ src/org/rascalmpl/eclipse/console/RascalScriptInterpreter.java
@@ -39,10 +39,8 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
