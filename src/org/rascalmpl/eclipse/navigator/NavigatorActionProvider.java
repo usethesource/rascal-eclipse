7c4700b0fc4950c0866bb72657c90a51e0be650d
Jurgen J. Vinju <jurgen@vinju.org>
2015-01-18 16:03:47 +0100

    simplify

--- src/org/rascalmpl/eclipse/navigator/NavigatorActionProvider.java
+++ src/org/rascalmpl/eclipse/navigator/NavigatorActionProvider.java
@@ -1,37 +1,27 @@
 package org.rascalmpl.eclipse.navigator;
 
-import java.net.URI;
-
-import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionConstants;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
 import org.eclipse.ui.navigator.ICommonMenuConstants;
 import org.eclipse.ui.navigator.ICommonViewerSite;
 import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
-import org.rascalmpl.eclipse.Activator;
 import org.rascalmpl.eclipse.editor.EditorUtil;
 import org.rascalmpl.eclipse.uri.URIStorage;
 
 public class NavigatorActionProvider extends CommonActionProvider {
 
   public class OpenFileStoreAction extends Action {
-    private final IWorkbenchPage page;
     private final ISelectionProvider sp;
     private URIStorage store;
 
-    public OpenFileStoreAction(IWorkbenchPage page, ISelectionProvider selectionProvider) {
-      this.page = page;
+    public OpenFileStoreAction(ISelectionProvider selectionProvider) {
       this.sp = selectionProvider;
     }
     
@@ -72,7 +62,7 @@ public class NavigatorActionProvider extends CommonActionProvider {
     ICommonViewerSite viewSite = aSite.getViewSite();
     if (viewSite instanceof ICommonViewerWorkbenchSite) {
       ICommonViewerWorkbenchSite workbenchSite = (ICommonViewerWorkbenchSite) viewSite;
-      openAction =  new OpenFileStoreAction(workbenchSite.getPage(), workbenchSite.getSelectionProvider());
+      openAction =  new OpenFileStoreAction(workbenchSite.getSelectionProvider());
     }
   } 
 
