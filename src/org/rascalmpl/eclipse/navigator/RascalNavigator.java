1568e4861db261aaf09f2e2c51e91cd971ebb69a
Jurgen J. Vinju <jurgen@vinju.org>
2015-01-24 08:45:25 +0100

    added TODOs for memento

--- src/org/rascalmpl/eclipse/navigator/RascalNavigator.java
+++ src/org/rascalmpl/eclipse/navigator/RascalNavigator.java
@@ -1,6 +1,7 @@
 package org.rascalmpl.eclipse.navigator;
 
 import java.util.ArrayList;
+
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -10,6 +11,7 @@ import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IWorkingSet;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.navigator.CommonNavigator;
+import org.rascalmpl.eclipse.uri.URIStorage;
 
 public class RascalNavigator extends CommonNavigator {
 	
@@ -17,13 +19,12 @@ public class RascalNavigator extends CommonNavigator {
 		super();
 	}
 	
-	@SuppressWarnings("unchecked")
 	private void restoreState() {
 		if (memento == null) {
 			return;
 		}
-		@SuppressWarnings("rawtypes")
-		ArrayList elements = new ArrayList();
+
+		ArrayList<Object> elements = new ArrayList<>();
 		IContainer container = ResourcesPlugin.getWorkspace().getRoot();
 		IMemento childMem = memento.getChild("expanded");
 		if (childMem != null) {
@@ -36,41 +37,53 @@ public class RascalNavigator extends CommonNavigator {
 			}
 			elementMem = childMem.getChildren("element");
 			for (int i = 0; i < elementMem.length; i++) {
-				Object element = container.findMember(elementMem[i]
-						.getString("path"));
-	            if (element != null) {
-	              elements.add(element);
-	            }
+				Object element = container.findMember(elementMem[i].getString("path"));
+				if (element != null) {
+					elements.add(element);
+				}
 			}
-	      }
+			
+			elementMem = childMem.getChildren("storage");
+			for (int i = 0; i < elementMem.length; i++) {
+				Object element = container.findMember(elementMem[i].getString("uri"));
+				if (element != null) {
+					elements.add(element);
+				}
+			}
+		}
 		getCommonViewer().setExpandedElements(elements.toArray());
-	  }
+	}
 	  
-	  @Override
-	  public void saveState(IMemento memento) {
-	      super.saveState(memento);
-		  Object expandedElements[] = ((TreeViewer)this.getCommonViewer()).getExpandedElements();
-	      if (expandedElements.length > 0) {
-	          IMemento expandedMem = memento.createChild("expanded");
-	          for (int i = 0; i < expandedElements.length; i++) {
-	              if (expandedElements[i] instanceof IResource) {
-	                  IMemento elementMem = expandedMem
-	                          .createChild("element");
-	                  elementMem.putString("path",
-	                          ((IResource) expandedElements[i]).getFullPath()
-	                                  .toString());
-	              }
-	              else if (expandedElements[i] instanceof IWorkingSet) {
-	            	  IMemento elementMem = expandedMem.createChild("ws_element");
-	            	  elementMem.putString("name", ((IWorkingSet) expandedElements[i]).getName());
-	              }
-	          }
-	      }
-	  }
-	  
-	  @Override
-	  public void createPartControl(Composite aParent) {
-		  super.createPartControl(aParent);
-		  restoreState();
-	  }
+	@Override
+	public void saveState(IMemento memento) {
+		super.saveState(memento);
+		Object expandedElements[] = ((TreeViewer)this.getCommonViewer()).getExpandedElements();
+		if (expandedElements.length > 0) {
+			IMemento expandedMem = memento.createChild("expanded");
+			for (int i = 0; i < expandedElements.length; i++) {
+				if (expandedElements[i] instanceof IResource) {
+					IMemento elementMem = expandedMem.createChild("element");
+					elementMem.putString("path", ((IResource) expandedElements[i]).getFullPath().toString());
+				}
+				else if (expandedElements[i] instanceof URIStorage) {
+					IMemento elementMem = expandedMem.createChild("storage");
+					elementMem.putString("uri", ((URIStorage) expandedElements[i]).getURI().toString());
+					// TODO: we need to find the project name with this in order to restore the
+					// URIResolverRegistry properly with this.
+				}
+				else if (expandedElements[i] instanceof IWorkingSet) {
+					IMemento elementMem = expandedMem.createChild("ws_element");
+					elementMem.putString("name", ((IWorkingSet) expandedElements[i]).getName());
+				}
+				// TODO: store the search path content, and save the
+				// associated project name with it. This should help.
+			}
+		}
+	}
+
+	@Override
+	public void createPartControl(Composite aParent) {
+		super.createPartControl(aParent);
+		restoreState();
+	}
 }
