2414772f187b98a152f0ae9f4d1f22c958a3542b
Jurgen J. Vinju <jurgen@vinju.org>
2015-06-09 21:56:30 +0200

    removed dead code

--- src/org/rascalmpl/eclipse/wizards/NewRascalFile.java
+++ src/org/rascalmpl/eclipse/wizards/NewRascalFile.java
@@ -13,18 +13,14 @@
 *******************************************************************************/
 package org.rascalmpl.eclipse.wizards;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.Writer;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.Charset;
 import java.util.List;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
@@ -33,7 +29,6 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.imp.pdb.facts.ISourceLocation;
@@ -49,7 +44,6 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 import org.rascalmpl.eclipse.IRascalResources;
-import org.rascalmpl.eclipse.editor.EditorUtil;
 import org.rascalmpl.eclipse.uri.ProjectURIResolver;
 import org.rascalmpl.eclipse.util.RascalEclipseManifest;
 import org.rascalmpl.eclipse.util.RascalKeywords;
@@ -168,11 +162,6 @@ public class NewRascalFile extends Wizard implements INewWizard {
 			}
 		});
 		monitor.worked(1);
-	}
-	
-	private InputStream openContentStream() {
-		String contents = "module "+moduleName;
-		return new ByteArrayInputStream(contents.getBytes());
 	}
 	
 	private void throwCoreException(String message) throws CoreException {
