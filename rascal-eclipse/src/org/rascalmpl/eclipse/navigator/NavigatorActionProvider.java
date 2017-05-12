c79bbac263dd9d6cd692eebccc530cce5203258d
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2017-05-12 22:24:52 +0200

    added copy loc in pathConfig browser

--- rascal-eclipse/src/org/rascalmpl/eclipse/navigator/NavigatorActionProvider.java
+++ rascal-eclipse/src/org/rascalmpl/eclipse/navigator/NavigatorActionProvider.java
@@ -9,17 +9,20 @@ import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionConstants;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
-import org.eclipse.ui.navigator.ICommonMenuConstants;
 import org.eclipse.ui.navigator.ICommonViewerSite;
 import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
 import org.rascalmpl.eclipse.editor.EditorUtil;
+import org.rascalmpl.eclipse.library.util.Clipboard;
 import org.rascalmpl.eclipse.navigator.NavigatorContentProvider.URIContent;
 import org.rascalmpl.uri.URIResolverRegistry;
+import org.rascalmpl.values.ValueFactoryFactory;
+
 import io.usethesource.vallang.ISourceLocation;
+import io.usethesource.vallang.IValueFactory;
 
 public class NavigatorActionProvider extends CommonActionProvider {
 
-  public class OpenFileStoreAction extends Action {
+  public static class OpenFileStoreAction extends Action {
     private final ISelectionProvider sp;
     private ISourceLocation store;
 
@@ -52,10 +55,45 @@ public class NavigatorActionProvider extends CommonActionProvider {
       }
       return false;
     }
-
   }
 
+  public static class CopySourceLocationAction extends Action {
+      private final ISelectionProvider sp;
+      private ISourceLocation store;
+      private final IValueFactory vf = ValueFactoryFactory.getValueFactory();
+      private final Clipboard cb = new Clipboard(vf);
+
+      public CopySourceLocationAction(ISelectionProvider selectionProvider) {
+        this.sp = selectionProvider;
+      }
+      
+      @Override
+      public String getText() {
+        return "Copy Location";
+      }
+      
+      @Override
+      public void run() {
+        if (store != null) {
+            cb.copy(store);
+        }
+      }
+      
+      public boolean isEnabled() {
+          ISelection selection = sp.getSelection();
+          if (!selection.isEmpty()) {
+            IStructuredSelection sSelection = (IStructuredSelection) selection;
+            if(sSelection.size() == 1 && sSelection.getFirstElement() instanceof URIContent) {
+              store = ((URIContent) sSelection.getFirstElement()).getURI();
+              return true;
+            }
+          }
+          return false;
+        }
+  }
+  
   private OpenFileStoreAction openAction;
+  private CopySourceLocationAction copyAction;
 
   public NavigatorActionProvider() {     
   }
@@ -65,18 +103,27 @@ public class NavigatorActionProvider extends CommonActionProvider {
     if (viewSite instanceof ICommonViewerWorkbenchSite) {
       ICommonViewerWorkbenchSite workbenchSite = (ICommonViewerWorkbenchSite) viewSite;
       openAction =  new OpenFileStoreAction(workbenchSite.getSelectionProvider());
+      copyAction = new CopySourceLocationAction(workbenchSite.getSelectionProvider());
     }
   } 
 
   public void fillActionBars(IActionBars actionBars) { 
     if (openAction.isEnabled()) {
       actionBars.setGlobalActionHandler(ICommonActionConstants.OPEN, openAction);
+      actionBars.getMenuManager().add(openAction);
+    }
+    
+    if (copyAction.isEnabled()) {
+        actionBars.getMenuManager().add(copyAction);
     }
   }
 
   public void fillContextMenu(IMenuManager menu) {
     if (openAction.isEnabled()) {
-      menu.appendToGroup(ICommonMenuConstants.GROUP_OPEN, openAction);
+      menu.add(openAction);
+    }
+    if (copyAction.isEnabled()) {
+        menu.add(copyAction); 
     }
   }
 }
