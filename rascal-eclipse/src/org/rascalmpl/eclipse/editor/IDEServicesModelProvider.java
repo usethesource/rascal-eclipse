5147457435c017c9794d5e964d8fcfea727085d4
Davy Landman <davy.landman@swat.engineering>
2018-11-22 15:09:59 +0100

    Revert "Added some debug logging around service provider"
    This reverts commit fd5ddaf6d17142a9c96021e786f69bf7bbb9e9be.

--- rascal-eclipse/src/org/rascalmpl/eclipse/editor/IDEServicesModelProvider.java
+++ rascal-eclipse/src/org/rascalmpl/eclipse/editor/IDEServicesModelProvider.java
@@ -46,7 +46,6 @@ public class IDEServicesModelProvider {
             kernel = Java2Rascal.Builder.bridge(vf, new PathConfig(), IKernel.class).build();
             IDESummaryService serviceToUse = getExtensionPointIDESummary();
             if (serviceToUse == null) {
-				Activator.log("Falling back to the default service provider", new RuntimeException());
             	// by default, use the kernel 
                 serviceToUse = new IDESummaryService() {
                     @Override
@@ -98,7 +97,6 @@ public class IDEServicesModelProvider {
 				}
 			} 
 		}
-		Activator.log("No extension point for service provider registered", new RuntimeException());
 		return null;
 	}
     
