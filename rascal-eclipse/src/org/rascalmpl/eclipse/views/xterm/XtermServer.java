c353255599bf4b111e553b9829f94563616fb66a
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-11-23 16:53:46 +0100

    removed dead stuff

--- rascal-eclipse/src/org/rascalmpl/eclipse/views/xterm/XtermServer.java
+++ rascal-eclipse/src/org/rascalmpl/eclipse/views/xterm/XtermServer.java
@@ -2,32 +2,22 @@ package org.rascalmpl.eclipse.views.xterm;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Map;
 
 import org.rascalmpl.uri.URIResolverRegistry;
 import org.rascalmpl.uri.URIUtil;
-import org.rascalmpl.values.ValueFactoryFactory;
 
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.NanoHTTPD.Response.Status;
 import io.usethesource.vallang.ISourceLocation;
-import io.usethesource.vallang.IValueFactory;
 
 public class XtermServer extends NanoHTTPD {
-    IValueFactory vf = ValueFactoryFactory.getValueFactory();
-    StringWriter outWriter;
-    PrintWriter outPrintWriter;
-    StringWriter errWriter;
-    PrintWriter errPrintWriter;
     private final int port;
 
     public XtermServer(int port, ISourceLocation root) throws IOException {
         super(port);
         this.port = port;
     }
-
 
     public int getPort() {
         return port;
@@ -43,7 +33,6 @@ public class XtermServer extends NanoHTTPD {
             return newFixedLengthResponse(Status.NOT_FOUND, "text/plain", uri + " not found.\n" + e);
         }
     }
-
 
     public OutputStream getRemoteToTerminalOutputStream() {
         return null;
