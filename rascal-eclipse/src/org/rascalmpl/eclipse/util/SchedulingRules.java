83055b4284a4c088a52319d72ba9ee738da49585
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2019-12-19 11:31:48 +0100

    fixed some bugs

--- rascal-eclipse/src/org/rascalmpl/eclipse/util/SchedulingRules.java
+++ rascal-eclipse/src/org/rascalmpl/eclipse/util/SchedulingRules.java
@@ -3,9 +3,9 @@ package org.rascalmpl.eclipse.util;
 import java.util.Arrays;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.MultiRule;
 import org.eclipse.jdt.core.JavaCore;
@@ -16,23 +16,23 @@ import org.rascalmpl.eclipse.IRascalResources;
 public class SchedulingRules {
     public static ISchedulingRule getRascalProjectsRule() {
         return new MultiRule(Arrays.stream(ResourcesPlugin.getWorkspace().getRoot().getProjects())
-                .filter(SchedulingRules::isRascalProject)
+                .filter(SchedulingRules::isOpenRascalProject)
                 .map(SchedulingRules::getBinFolder)
                 .toArray(ISchedulingRule[]::new));
     }
     
-    private static IPath getBinFolder(IProject p) {
+    private static IResource getBinFolder(IProject p) {
         try {
-            return JavaCore.create(p).getOutputLocation();
+            return p.getFolder(JavaCore.create(p).getOutputLocation());
         } catch (JavaModelException e) {
             Activator.log("could not get bin folder", e);
-            return p.getFullPath();
+            return p;
         }
     }
 
-    private static boolean isRascalProject(IProject p) {
+    private static boolean isOpenRascalProject(IProject p) {
         try {
-            return p.hasNature(IRascalResources.ID_RASCAL_NATURE);
+            return p.isOpen() && p.hasNature(IRascalResources.ID_RASCAL_NATURE);
         } catch (CoreException e) {
             Activator.log("unexpected error while checking for Rascal project", e);
             return false;
